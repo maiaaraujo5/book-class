11a3b38fa97ac182714ba724daceaf4a74f506f4
Lucas <maia.araujo5@gmail.com>
2021-09-22 21:59:13 -0300

    test:create test to controller

--- src/test/java/com/maiaaraujo5/bookclass/controller/createTeacher/CreateTeacherControllerTest.java
+++ src/test/java/com/maiaaraujo5/bookclass/controller/createTeacher/CreateTeacherControllerTest.java
@@ -1,12 +1,14 @@
 package com.maiaaraujo5.bookclass.controller.createTeacher;
 
 import com.maiaaraujo5.bookclass.domain.teacher.Teacher;
+import com.maiaaraujo5.bookclass.exception.TeacherAlreadyExists;
 import com.maiaaraujo5.bookclass.service.createTeacher.CreateTeacher;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -42,4 +44,16 @@ class CreateTeacherControllerTest {
                 .andExpect(jsonPath("$.lastname", is("Doe")))
                 .andExpect(jsonPath("$.email", is("johndoe@johndoe.com")));
     }
+
+    @Test
+    void should_return_status_conflict_when_service_throws_teacher_already_exists() throws Exception {
+        given(createTeacher.Execute(any())).willThrow(new TeacherAlreadyExists("This teacher already exists"));
+        mockMvc.perform(post("/v1/teacher")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"John\",\"lastname\":\"Doe\",\"email\":\"johndoe@johndoe.com\"}"))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.status", is(HttpStatus.CONFLICT.name())))
+                .andExpect(jsonPath("$.message", is("This teacher already exists")));
+    }
+
 }
